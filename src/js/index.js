d4bcebae0661cb75cecbcf26a1224ec090ed3bdd
grindpride <evil.koprof@gmail.com>
2018-05-02 22:40:13 +0300

    burger style

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,20 @@ $('.player, .player-profile__close').click(function(){
 	$('.player-profile').toggleClass('visible');
 
 });
-
-$('.tab').click(function(){
-	$('.tab').removeClass('active');
+$('.burger').click(function(){
+	console.log('toggle');
 	$(this).toggleClass('active');
-})
+	$('.menu').toggleClass('active');
+
+});
+$("ul.tabs__caption").on("click", "li:not(.active)", function() {
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active")
+    .closest("div.tabs")
+    .find("div.tabs__content")
+    .removeClass("active")
+    .eq($(this).index())
+    .addClass("active");
+});
